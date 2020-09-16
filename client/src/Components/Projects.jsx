9c8f5aaf00f28a137ca2f880a05f7df0f94e1738
Colin Malcolm <colinmalcolm8@gmail.com>
2020-09-16 13:57:14 -0400

    added spotify app card and made desc edits

--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -5,6 +5,7 @@ import Schmedium from '../assets/Schmedium.png'
 import github from '../assets/github.svg'
 import interweb from '../assets/interweb.svg'
 import Hello from '../assets/Hello.jpg'
+import spotify from '../assets/Spotify.png'
 
 
 
@@ -19,14 +20,14 @@ export default class Projects extends Component {
                     <div class="px-6 py-4">
                         <h1 className="font-bold">Greedos</h1>
                         <p class="text-gray-700 text-base">
-                            Greedos is an intergalactic bounty board for aspiring and experienced bounty hunters across the galaxy. This website shows a list of star wars characters and the information on each of the characters including, eye color, weight, name and their bounty.
+                            Greedos is a fun, engaging, intergalactic bounty board for aspiring and experienced bounty hunters across the universe. This website shows Star Wars wanted characters and the information on each including their name and their bounty.
                 </p>
                     </div>
                     <div class="px-6 py-4">
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">React</span>
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">JavaScript</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">CSS</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">SWAPI-API</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">CSS</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">SWAPI-API</span>
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700" >Netlify</span>
                         <div className="flex flex-wrap justify-between">
                             <a href="https://github.com/malcolmc10/GreedosBB/blob/master/README.md"><img src={github} className="h-6 w-6" /></a>
@@ -50,10 +51,10 @@ export default class Projects extends Component {
                     <div class="px-6 py-4">
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">React</span>
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">MongoDB</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Express</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Heroku</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Surge</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">TailwindCSS</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Express</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Heroku</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Surge</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">TailwindCSS</span>
                         <div className="flex flex-wrap justify-between">
                         <a href="https://github.com/malcolmc10/P3-recipebox/blob/dev/p3-worksheet.md"><img src={github} className="h-6 w-6" /></a>
                         <a href="http://foodiesandgoodiesrecipebox.surge.sh/"><img src={interweb} className="h-6 w-6" /></a>
@@ -76,10 +77,10 @@ export default class Projects extends Component {
                     <div class="px-6 py-4">
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">React</span>
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">TailwindCSS</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Ruby on Rails</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">PostgreSQL</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Heroku</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Surge</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Ruby on Rails</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">PostgreSQL</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Heroku</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Surge</span>
                         
                         <div className="flex flex-wrap justify-between">
                             <a href="https://github.com/malcolmc10/Schmedium"><img src={github} className="h-6 w-6" /></a>
@@ -101,8 +102,8 @@ export default class Projects extends Component {
                     </div>
                     <div class="px-6 py-4">
                         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">React</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Surge</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">TailwindCSS</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Surge</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">TailwindCSS</span>
                         <div className="flex flex-wrap justify-between">
                             <a href="https://github.com/malcolmc10/Portfolio"><img src={github} className="h-6 w-6" /></a>
                             <a href="http://iamcolinmalcolm.surge.sh/"><img src={interweb} className="h-6 w-6" /></a>
@@ -112,8 +113,31 @@ export default class Projects extends Component {
                 </div>
 
 
+                {/* project 5 */}
 
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white ml-2 mt-4">
+                <a href="http://spotifyme.surge.sh/"><img class="w-full" src={spotify} alt="Sunset in the mountains"></img></a>
+                    <div class="px-6 py-4">
+                        <h1 className="font-bold">Spotify UI Clone</h1>
+                        <p class="text-gray-700 text-base">
+                            This application is a UI clone of the popular music streaming app Spotify. Using React, React Router, Javascript, and Sass I wanted to create something I loved but add features that make it my own. I hope you enjoy my passion project.
+                </p>
+                    </div>
+                    <div class="px-6 py-4">
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">React</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Sass</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">JavaScript</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">HTML</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">React Router</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Surge</span>
+                        
+                        <div className="flex flex-wrap justify-between">
+                            <a href="https://github.com/malcolmc10/Schmedium"><img src={github} className="h-6 w-6" /></a>
+                            <a href="http://schmediumpotter.surge.sh/"><img src={interweb} className="h-6 w-6" /></a>
+                        </div>
+                    </div>
 
+                </div>
 
 
 
