1812026b69900ebe915dc1f92dbd98cb347d68ad
Colin Malcolm <colinmalcolm8@gmail.com>
2020-09-19 14:20:18 -0400

    fixed breakpoint margins on projects

--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -15,7 +15,7 @@ export default class Projects extends Component {
             <div className="items-center text-center  flex flex-wrap justify-evenly" >
                 {/* project 1 */}
 
-                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mr-2 mt-4">
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mr-0 mt-4 sm:mr-2 mt-4">
                     <a href="https://greedosbb.netlify.app/"><img class="w-full" src={Greedos} alt="Sunset in the mountains"></img></a>
                     <div class="px-6 py-4">
                         <h1 className="font-bold">Greedos</h1>
@@ -66,7 +66,7 @@ export default class Projects extends Component {
 
                 {/* project 3 */}
 
-                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white ml-2 mt-4">
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white ml-0 mt-4 sm:ml-2 mt-4">
                 <a href="http://schmediumpotter.surge.sh/"><img class="w-full" src={Schmedium} alt="Sunset in the mountains"></img></a>
                     <div class="px-6 py-4">
                         <h1 className="font-bold">Schmedium</h1>
@@ -92,7 +92,7 @@ export default class Projects extends Component {
 
                 {/* Project 4 */}
 
-                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mr-2  mt-4">
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mr-0 mt-4 sm:mr-2  mt-4">
                 <a href="ttp://iamcolinmalcolm.surge.sh/"><img class="w-full" src={Hello} alt="Sunset in the mountains"></img></a>
                     <div class="px-6 py-4">
                         <h1 className="font-bold">Colin's Portfolio</h1>
@@ -115,7 +115,7 @@ export default class Projects extends Component {
 
                 {/* project 5 */}
 
-                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white ml-2 mt-4">
+                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white ml-0 mt-4 sm:ml-2 mt-4">
                 <a href="http://spotifyme.surge.sh/"><img class="w-full" src={spotify} alt="Sunset in the mountains"></img></a>
                     <div class="px-6 py-4">
                         <h1 className="font-bold">Spotify UI Clone</h1>
