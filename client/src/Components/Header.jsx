6d104c480458c62125a025412bc0c9f0efd8039e
Colin Malcolm <colinmalcolm8@gmail.com>
2020-09-19 15:26:32 -0400

    playing with font sizes for header

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -11,23 +11,22 @@ export default class Header extends Component {
     <a href="/"><span className="font-semibold text-xl tracking-tight mr-4 hidden sm:inline-block">Colin Malcolm</span></a>
   
   
-  {/* <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto"> */}
-    {/* <div className="text-sm lg:flex-grow"> */}
-      <a href="#About" className=" font-bold  text-teal-400 hover:text-pink-400 mr-4 text-sm sm:text-base">
+  
+      <a href="#About" className=" font-bold  text-teal-400 hover:text-pink-400 mr-4 text-xs sm:text-base">
         Who Am I?
       </a>
-      <a href="#Title" className=" font-bold text-yellow-400 hover:text-teal-400 mr-4 text-sm sm:text-base">
+      <a href="#Title" className=" font-bold text-yellow-400 hover:text-teal-400 mr-4 text-xs sm:text-base">
         Projects
       </a>
-      <a href="https://pdfhost.io/v/pax7BNZQ6_Colin_Malcolm_SWE_Resume.pdf" className=" font-bold  text-pink-400 hover:text-indigo-400 text-sm sm:text-base">
+      <a href="https://pdfhost.io/v/pax7BNZQ6_Colin_Malcolm_SWE_Resume.pdf" className=" font-bold  text-pink-400 hover:text-indigo-400 text-xs sm:text-base">
         Resume
       </a>
       </div>
-    {/* </div> */}
+    
     <div>
       <a href="#MyForm" class="inline-block font-bold text-sm px-4 py-2 leading-none border rounded text-pink-400 border-teal-400 hover:border-pink-400 hover:text-teal-400 hover:bg-black text-xs sm:text-base">Contact Me</a>
     </div>
-  {/* </div> */}
+  
   
 </nav>
 </div>        
